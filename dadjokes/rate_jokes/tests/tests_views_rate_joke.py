59b109ef8ddc9c51570edc030b12632462c36450
Cam McHugh <cam.mchugh@ncsmartcall.com>
2021-01-28 15:54:53 -0600

    mocking icanhasjoke requests

--- dadjokes/rate_jokes/tests/tests_views_rate_joke.py
+++ dadjokes/rate_jokes/tests/tests_views_rate_joke.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
-from unittest import skip
+from unittest.mock import patch, Mock
 
 from ..models import DadJoke, Rating
 
@@ -11,20 +11,33 @@ class RateJokeViewGetTests(TestCase):
         # setup
         cam = User.objects.create_user('cam', password='password')
         self.client.force_login(cam)
-        # execute
-        response = self.client.get('/rate/')
+
+        with patch('requests.get') as request_mock:
+            mock_response = Mock()
+            mock_response.configure_mock(**{
+                'json.return_value': {'id': 'abc123', 'joke': 'This is a joke.'},
+                'status_code': 200
+            })
+            request_mock.return_value = mock_response
+            # execute
+            response = self.client.get('/rate/')
         # assert
-        new_joke = DadJoke.objects.get()
-        self.assertContains(response, new_joke.joke_text)
+        self.assertTrue(DadJoke.objects.filter(joke_reference_id='abc123').exists())
+        self.assertContains(response, 'This is a joke.')
 
     def test_get_uses_existing_unrated_joke_instead_of_fetching(self):
         # setup
         cam = User.objects.create_user('cam', password='password')
         existing_joke = DadJoke.objects.create(joke_reference_id='abc123', joke_text='A duck walks into a bar...')
         self.client.force_login(cam)
+
         # execute
-        response = self.client.get('/rate/')
+        with patch('requests.get') as request_mock:
+            response = self.client.get('/rate/')
+
         # assert
+        request_mock.assert_not_called()
+        self.assertFalse(DadJoke.objects.exclude(joke_reference_id='abc123').exists())
         self.assertContains(response, existing_joke.joke_text)
 
     def test_get_fetching_new_joke_when_user_has_rated_all_exsting_jokes(self):
@@ -33,24 +46,47 @@ class RateJokeViewGetTests(TestCase):
         existing_joke = DadJoke.objects.create(joke_reference_id='abc123', joke_text='A duck walks into a bar...')
         Rating.objects.create(dad_joke=existing_joke, rated_by=cam, rating_value='giggle')
         self.client.force_login(cam)
-        # execute
-        response = self.client.get('/rate/')
-        # assert
-        new_joke = DadJoke.objects.all().order_by('pk')[1]
-        self.assertContains(response, new_joke.joke_text[:10])
 
-    @skip('Not actually possible yet.')
-    def test_get_retries_fetching_new_joke_once_when_existing_joke_returned(self):
+        with patch('requests.get') as request_mock:
+            mock_response = Mock()
+            mock_response.configure_mock(**{
+                'json.return_value': {'id': 'def456', 'joke': 'This is a joke.'},
+                'status_code': 200
+            })
+            request_mock.return_value = mock_response
+            # execute
+            response = self.client.get('/rate/')
+
+        # assert
+        self.assertTrue(DadJoke.objects.filter(joke_reference_id='def456').exists())
+        self.assertContains(response, 'This is a joke.')
+
+    def test_get_retries_fetching_new_joke_once_when_existing_rated_joke_returned(self):
         # setup
         cam = User.objects.create_user('cam', password='password')
-        existing_joke = DadJoke.objects.create(joke_reference_id='abc123', joke_text='A duck walks into a bar...')
+        existing_joke = DadJoke.objects.create(joke_reference_id='abc123', joke_text='This is an existing joke.')
         Rating.objects.create(dad_joke=existing_joke, rated_by=cam, rating_value='giggle')
         self.client.force_login(cam)
-        # execute
-        response = self.client.get('/rate/')
+
+        with patch('requests.get') as request_mock:
+            duplicate_joke_response = Mock()
+            duplicate_joke_response.configure_mock(**{
+                'json.return_value': {'id': 'abc123', 'joke': 'This is a existing joke.'},
+                'status_code': 200
+            })
+            new_joke_response = Mock()
+            new_joke_response.configure_mock(**{
+                'json.return_value': {'id': 'def456', 'joke': 'This is a new joke.'},
+                'status_code': 200
+            })
+            request_mock.side_effect = [duplicate_joke_response, new_joke_response]
+
+            # execute
+            response = self.client.get('/rate/')
+
         # assert
-        new_joke = DadJoke.objects.all().order_by('pk')[1]
-        self.assertContains(response, new_joke.joke_text[:10])
+        self.assertTrue(DadJoke.objects.filter(joke_reference_id='def456').exists())
+        self.assertContains(response, 'This is a new joke.')
 
 
 class RateJokeViewPostTests(TestCase):
@@ -61,9 +97,9 @@ class RateJokeViewPostTests(TestCase):
         existing_joke = DadJoke.objects.create(joke_reference_id='abc123', joke_text='A duck walks into a bar...')
         self.client.force_login(cam)
         # execute
-        response = self.client.post('/rate/', data={'dad_joke_id': existing_joke.id, 'rating': 'giggle'})
+        response = self.client.post('/rate/', data={'dad_joke_id': existing_joke.id, 'rating': '3_giggle'})
         # assert
-        self.assertTrue(Rating.objects.filter(rated_by=cam).exists())
+        self.assertTrue(Rating.objects.filter(dad_joke=existing_joke, rated_by=cam).exists())
         self.assertRedirects(response, '/rate/')
 
     def test_post_does_not_create_new_rating_if_invalid_rating_and_shows_error(self):
@@ -72,17 +108,17 @@ class RateJokeViewPostTests(TestCase):
         existing_joke = DadJoke.objects.create(joke_reference_id='abc123', joke_text='A duck walks into a bar...')
         self.client.force_login(cam)
         # execute
-        response = self.client.post('/rate/', data={'dad_joke_id': existing_joke.id, 'rating': 'gaggle'})
+        response = self.client.post('/rate/', data={'dad_joke_id': existing_joke.id, 'rating': '3_gaggle'})
         # assert
         self.assertFalse(Rating.objects.filter(rated_by=cam).exists())
-        self.assertContains(response, 'Select a valid choice. gaggle is not one of the available choices.')
+        self.assertContains(response, 'Select a valid choice. 3_gaggle is not one of the available choices.')
 
     def test_post_does_not_create_new_rating_if_invalid_rating_joke_id_and_redirects(self):
         # setup
         cam = User.objects.create_user('cam', password='password')
         self.client.force_login(cam)
         # execute
-        response = self.client.post('/rate/', data={'dad_joke_id': 100, 'rating': 'giggle'})
+        response = self.client.post('/rate/', data={'dad_joke_id': 100, 'rating': '3_giggle'})
         # assert
         self.assertFalse(Rating.objects.filter(rated_by=cam).exists())
         self.assertRedirects(response, '/rate/')
