92cee5ef427181fdceac3048b5a3ada37c074c94
Cam McHugh <cam.mchugh@ncsmartcall.com>
2021-01-28 18:53:21 -0600

    pushed persistence details into model class

--- dadjokes/rate_jokes/joke_fetcher.py
+++ dadjokes/rate_jokes/joke_fetcher.py
@@ -6,14 +6,13 @@ class JokeFetcher(object):
 
     @classmethod
     def get_joke(cls, user, get_joke_fn=random_joke):
-        joke = DadJoke.objects.exclude(ratings__rated_by=user).first()
+        joke = DadJoke.unrated_joke_for_user(user)
         if not joke:
             for _ in range(2):
                 i_can_has_dad_joke = get_joke_fn()
-                joke, created = DadJoke.objects.get_or_create(
-                    joke_reference_id=i_can_has_dad_joke.joke_id,
-                    defaults={'joke_text': i_can_has_dad_joke.joke_text}
-                )
-                if created and not user.rated_jokes.filter(dad_joke=joke).exists():
-                    break
+                joke, created = DadJoke.save_joke(i_can_has_dad_joke)
+                # if the joke was new to us, return it.
+                # Otherwise it's a repeat of a previous, rated joke, try one more time for a new joke.
+                if created:
+                    return joke
         return joke
--- dadjokes/rate_jokes/tests/tests_joke_fetcher.py
+++ dadjokes/rate_jokes/tests/tests_joke_fetcher.py
@@ -1,61 +1,56 @@
-from django.test import TestCase
-from django.contrib.auth.models import User
+from django.test import SimpleTestCase
 from unittest.mock import patch, Mock
 
-from ..models import DadJoke, Rating
 from ..joke_fetcher import JokeFetcher
 
 
-class JokeFetcherTests(TestCase):
+class JokeFetcherTests(SimpleTestCase):
 
     def setUp(self):
         super(JokeFetcherTests, self).setUp()
-        self.mock_random_joke = Mock()
-        self.mock_random_joke.return_value = Mock(joke_id='abc123', joke_text='This is a joke.')
+        self.joke_from_api = Mock()
+        self.mock_random_joke_fn = Mock()
+        self.mock_random_joke_fn.return_value = self.joke_from_api
+        self.mock_user = Mock()
 
-    def test_joke_fetcher_gets_joke(self):
+    @patch('rate_jokes.joke_fetcher.DadJoke')
+    def test_joke_fetcher_gets_joke(self, mock_dad_joke_cls):
         # setup
-        user = User.objects.create_user('cam')
+        new_joke = Mock()
+        mock_dad_joke_calls = {
+            'unrated_joke_for_user.return_value': None,
+            'save_joke.return_value': (new_joke, True)
+        }
+        mock_dad_joke_cls.configure_mock(**mock_dad_joke_calls)
         # execute
-        joke = JokeFetcher.get_joke(user, get_joke_fn=self.mock_random_joke)
+        actual_joke = JokeFetcher.get_joke(self.mock_user, get_joke_fn=self.mock_random_joke_fn)
         # assert
-        self.assertIsNotNone(joke.id)
-        self.assertEqual('abc123', joke.joke_reference_id)
-        self.assertEqual('This is a joke.', joke.joke_text)
+        mock_dad_joke_cls.save_joke.assert_called_with(self.joke_from_api)
+        self.assertEqual(actual_joke, new_joke)
 
-    def test_joke_fetcher_returns_existing_unrated_joke_instead_of_fetching(self):
+    @patch('rate_jokes.joke_fetcher.DadJoke')
+    def test_joke_fetcher_returns_existing_unrated_joke_instead_of_fetching(self, mock_dad_joke_cls):
         # setup
-        user = User.objects.create_user('cam')
-        DadJoke.objects.create(joke_reference_id='abc123', joke_text='This is a joke.')
+        existing_joke = Mock()
+        mock_dad_joke_calls = {'unrated_joke_for_user.return_value': existing_joke}
+        mock_dad_joke_cls.configure_mock(**mock_dad_joke_calls)
         # execute
-        joke = JokeFetcher.get_joke(user, get_joke_fn=self.mock_random_joke)
+        actual_joke = JokeFetcher.get_joke(self.mock_user, get_joke_fn=self.mock_random_joke_fn)
         # assert
-        self.mock_random_joke.assert_not_called()
-        self.assertEqual('abc123', joke.joke_reference_id)
+        self.mock_random_joke_fn.assert_not_called()
+        mock_dad_joke_cls.save_joke.assert_not_called()
+        self.assertEqual(existing_joke, actual_joke)
 
-    def test_joke_fetcher_fetches_new_joke_when_all_existing_have_been_rated(self):
+    @patch('rate_jokes.joke_fetcher.DadJoke')
+    def test_joke_fetcher_retries_if_it_gets_an_existing_rated_joke(self, mock_dad_joke_cls):
         # setup
-        user = User.objects.create_user('cam')
-        existing_joke = DadJoke.objects.create(joke_reference_id='def456', joke_text='This is a old joke.')
-        Rating.objects.create(dad_joke=existing_joke, rated_by=user, rating_value='asdf')
+        expected_joke = Mock()
+        mock_dad_joke_calls = {
+            'unrated_joke_for_user.side_effect': [None, None],
+            'save_joke.side_effect': [(Mock(), False), (expected_joke, True)]
+        }
+        mock_dad_joke_cls.configure_mock(**mock_dad_joke_calls)
         # execute
-        joke = JokeFetcher.get_joke(user, get_joke_fn=self.mock_random_joke)
+        actual_joke = JokeFetcher.get_joke(self.mock_user, self.mock_random_joke_fn)
         # assert
-        self.assertEqual('abc123', joke.joke_reference_id)
-        self.assertEqual('This is a joke.', joke.joke_text)
-
-    def test_joke_fetcher_retries_if_it_gets_an_existing_rated_joke(self):
-        # setup
-        user = User.objects.create_user('cam')
-        existing_joke = DadJoke.objects.create(joke_reference_id='abc123', joke_text='This is a old joke.')
-        Rating.objects.create(dad_joke=existing_joke, rated_by=user, rating_value='asdf')
-        mock_random_joke = Mock()
-        mock_random_joke.side_effect = [
-            Mock(joke_id='abc123', joke_text='This is an old joke.'),
-            Mock(joke_id='def456', joke_text='This is a new joke.')
-        ]
-        # execute
-        joke = JokeFetcher.get_joke(user, mock_random_joke)
-        # assert
-        self.assertEqual('def456', joke.joke_reference_id)
-        self.assertEqual('This is a new joke.', joke.joke_text)
+        self.assertEqual(expected_joke, actual_joke)
