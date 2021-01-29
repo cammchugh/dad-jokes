47b21424f7e3255d7aba564b36011b7224e2ef61
Cam McHugh <cam.mchugh@ncsmartcall.com>
2021-01-28 19:21:35 -0600

    cleanup, comment out post tests

--- dadjokes/rate_jokes/views/my_ratings.py
+++ dadjokes/rate_jokes/views/my_ratings.py
@@ -1,6 +1,4 @@
 import itertools
-
-from collections import defaultdict
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from django.shortcuts import render
@@ -17,14 +15,10 @@ class MyRatingsView(View):
         ratings = Rating.objects.filter(rated_by=user)
         sorted_ratings = sorted(ratings, key=lambda x: x.rating_value)
         grouped_ratings = []
-        # for rating_name, ratings in itertools.groupby(sorted_ratings, lambda x: x.rating_value):
-        #     display_name = [choice[1] for choice in rating_choices if choice[0] == rating_name][0]
-        #     grouped_ratings[display_name].extend(list(ratings))
         for rating_name, ratings in itertools.groupby(sorted_ratings, lambda x: RatingValue(x.rating_value).label):
             grouped_rating = (rating_name, list(ratings))
             grouped_ratings.append(grouped_rating)
         context = {
-            # can't iterate over default dict in templates.
             'grouped_ratings': grouped_ratings,
         }
         return render(request, 'rate_jokes/my_ratings.html', context)
--- dadjokes/rate_jokes/views/rate_joke.py
+++ dadjokes/rate_jokes/views/rate_joke.py
@@ -1,5 +1,3 @@
-import requests
-
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from django.shortcuts import render, redirect, reverse
